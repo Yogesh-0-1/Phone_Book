f50ee93bfb3eab8d4314167e6907c3b10684cc6b
Yogesh <sainiyogesh2101@gmail.com>
2022-06-28 12:08:24 +0530

    Defining ViewSets

--- backend/Phone_Book_App/CRUD/views.py
+++ backend/Phone_Book_App/CRUD/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import Form_Details
+from .serializers import Seriaizer
+from rest_framework import viewsets
 
 # Create your views here.
+class MovieViewSet(viewsets.ModelViewSet):
+    serializer_class = Seriaizer
+    queryset = Form_Details.objects.all()
