0432b9c38ca768fb36507bdfb68638f934b7cbf9
Yogesh <sainiyogesh2101@gmail.com>
2022-06-28 20:44:48 +0530

    Adding phone Number Field in Database

--- backend/Phone_Book_App/CRUD/admin.py
+++ backend/Phone_Book_App/CRUD/admin.py
@@ -2,5 +2,5 @@ from django.contrib import admin
 from .models import Form_Details
 
 class User_admin(admin.ModelAdmin):
-    list_display=( 'id','First_Name','Last_Name')
+    list_display=( 'id','First_Name','Last_Name','Phone_Number')
     admin.site.register(Form_Details)
--- backend/Phone_Book_App/CRUD/models.py
+++ backend/Phone_Book_App/CRUD/models.py
@@ -4,7 +4,8 @@ from django.db import models
 
 class Form_Details(models.Model ):
     First_Name=models.CharField(max_length=100)
-    Last_Name=models.CharField(max_length=100) 
+    Last_Name=models.CharField(max_length=100)
+    Phone_Number=models.CharField(max_length=10) 
     
     def __str__(self):
         return self.First_Name
